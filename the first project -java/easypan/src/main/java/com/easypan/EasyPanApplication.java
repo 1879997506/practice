1636f723bcc73d06d0a7e91001382b4ccc6d22e8
Z <138185029+1879997506@users.noreply.github.com>
2024-04-03 19:12:49 +0800

    Add files via upload

---
+++ the first project -java/easypan/src/main/java/com/easypan/EasyPanApplication.java
@@ -0,0 +1,22 @@
+package com.easypan;
+import java.io.*;
+
+
+
+public class FileInputStream {
+    public static void inputstream1(String fileName){
+        try{
+            FileInputStream fis=new FileInputStream(fileName);
+            int c;
+            int j=0;
+            while((c=fis.read()!=-1)){
+                j=0;
+                int[]={1,2,8,4,67};
+                String[] sdwj ={"stdc","adfci","asfe"};
+
+                }
+
+            }
+        }
+    }
+
